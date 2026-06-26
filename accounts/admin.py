aa7500824a4159d1fbb55680bb679aca74c1debf
xern <xern73@qq.com>
2026-06-26 10:44:09 +0800

    fix: 修复 Django 5.x 管理员创建用户时 usable_password 字段报错
    - BlogUserCreationForm 改为继承 UserCreationForm，移除重复的密码字段声明
    - 移除 BlogUserChangeForm 中过时的 exclude=('usable_password',)
    - 覆盖 BlogUserAdmin.add_fieldsets 移除父类中的 usable_password 字段
      （Django 5.2.1 的 UserAdmin.add_fieldsets 包含 usable_password，
       但 BlogUser 模型没有该字段，导致 modelform_factory 校验失败）

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.forms import UsernameField
 from django.utils.translation import gettext_lazy as _
 
@@ -8,26 +8,14 @@ from django.utils.translation import gettext_lazy as _
 from .models import BlogUser
 
 
-class BlogUserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label=_('password'), widget=forms.PasswordInput)
-    password2 = forms.CharField(label=_('Enter password again'), widget=forms.PasswordInput)
-
+class BlogUserCreationForm(UserCreationForm):
     class Meta:
         model = BlogUser
-        fields = ('email',)
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(_("passwords do not match"))
-        return password2
+        fields = ('email', 'username')
+        field_classes = {'username': UsernameField}
 
     def save(self, commit=True):
-        # Save the provided password in hashed format
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.source = 'adminsite'
             user.save()
@@ -37,7 +25,7 @@ class BlogUserCreationForm(forms.ModelForm):
 class BlogUserChangeForm(UserChangeForm):
     class Meta:
         model = BlogUser
-        exclude = ('usable_password',)
+        fields = '__all__'
         field_classes = {'username': UsernameField}
 
     def __init__(self, *args, **kwargs):
@@ -47,6 +35,12 @@ class BlogUserChangeForm(UserChangeForm):
 class BlogUserAdmin(UserAdmin):
     form = BlogUserChangeForm
     add_form = BlogUserCreationForm
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2"),
+        }),
+    )
     list_display = (
         'id',
         'nickname',
